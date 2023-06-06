ca25f5df43234787adceafab07b5b080a182737d
Hadiuzzaman Hadi <zhhadi50@gmail.com>
2023-06-06 19:29:13 +0600

    First Yew app run yew::Renderer::<App>::new().render();

--- 01 GetStart/src/main.rs
+++ 01 GetStart/src/main.rs
@@ -1,21 +1,10 @@
 use yew::prelude::*;
 
-#[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
 
+#[function_component(App)]
+fn app()-> Html {
     html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
+        <h2>{"To day is day for yew learning ....."}</h2>
     }
 }
 
